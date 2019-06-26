10bbefc863b24c17f2d75696a87998521dc5dfea
Jhon Pedroza <jhonfpedroza@gmail.com>
2019-06-26 02:42:11 -0500

    Detect battery number

--- tmux_scripts/battery.sh
+++ tmux_scripts/battery.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-capacity=$(cat /sys/class/power_supply/BAT1/capacity)
+if [ -f /sys/class/power_supply/BAT0/capacity ]; then
+    capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+elif [ -f /sys/class/power_supply/BAT1/capacity ]; then
+    capacity=$(cat /sys/class/power_supply/BAT1/capacity)
+fi
 
 if [ "$1" = "color" ]; then
     if [ "$capacity" -gt 15 ]; then
@@ -9,6 +13,6 @@ if [ "$1" = "color" ]; then
         echo "red"
     fi
 else
-    echo $capacity
+    echo "$capacity"
 fi
 
