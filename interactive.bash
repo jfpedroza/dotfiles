a025492ca520d7cfa2907343a67baf6494ba7792
Jhon Pedroza <jhonfpedroza@gmail.com>
2019-09-05 01:52:02 -0500

    WIP: Interactive installer

--- interactive.bash
+++ interactive.bash
@@ -42,17 +42,28 @@ die() {
 }
 
 should_do() {
-    echo "1: $1 - Everything: $everything"
     [[ -n "$1" || -n "$everything" ]] && return 0
 
     while true; do
-        read -p "$2 (Y/n)" answer
+        read -p "$2 (Y/n) " answer
         case $answer in
-            Y|y|yes) return 0 ;;
+            Y|y|yes|"") return 0 ;;
             N|n|no) return 1 ;;
             *) err "Invalid answer" ;;
         esac
     done
+}
+
+verify_distro() {
+    if [[ -r /etc/os-release ]]; then
+      distro=$(awk -F'=' '"NAME" == $1 { gsub("\"", "", $2); print tolower($2); }' /etc/os-release)
+      distro="${distro%% *}"
+    fi
+
+    case "$distro" in
+      manjaro|ubuntu) color green "Found distro: $distro"; return 0 ;;
+      *) die "Unknown distro :(" ;;
+    esac
 }
 
 while getopts 'abh' opt; do
@@ -66,14 +77,10 @@ done
 
 shift "$((OPTIND-1))"
 
-if [[ -r /etc/os-release ]]; then
-  distro=$(awk -F'=' '"NAME" == $1 { gsub("\"", "", $2); print tolower($2); }' /etc/os-release)
-  distro="${distro%% *}"
-fi
-
 main() {
-    echo "Distro: $distro"
-    (should_do $binaries "Install binary scripts?") && echo "Installing binaries"
+    verify_distro
+
+    (should_do "$binaries" "Install binary scripts?") && echo "Installing binaries"
 
     return 0
 }
