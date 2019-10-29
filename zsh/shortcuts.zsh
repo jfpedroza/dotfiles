7ffa6ddcffa9a5aba83c0f29d49a4a87b1feeb1c
Jhon Pedroza <jhonfpedroza@gmail.com>
2019-10-29 14:35:00 -0500

    Add private dotfiles

--- zsh/shortcuts.zsh
+++ zsh/shortcuts.zsh
@@ -5,26 +5,3 @@ export work=~/Work
 export programming=~/Programming
 export code=~/code
 
-if [ -d $work/Peiky ]; then
-    export peiky=$work/Peiky
-elif [ -d ~/Peiky ]; then
-    export peiky=~/Peiky
-fi
-
-if [ -d $work/Liftit ]; then
-    export liftit=$work/Liftit
-elif [ -d ~/Liftit ]; then
-    export liftit=~/Liftit
-fi
-
-if [ "$peiky" != "" ]; then
-    export peiky_and=$peiky/peiky-android
-    export peiky_core=$peiky/peiky-core
-    export peiky_copy=$peiky/peiky-core-copy
-fi
-
-if [ "$liftit" != "" ]; then
-    export liftit_core=$liftit/core
-    export liftit_copy=$liftit/core-copy
-fi
-
