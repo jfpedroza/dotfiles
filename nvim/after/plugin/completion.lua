042b25364fe7a8984007ad3aa511a96dd9ec2308
Jhon Pedroza <jhon@pedroza.me>
2022-09-03 04:38:54 -0500

    [vim] Set cmp-buffer to complete from all visible buffers

--- nvim/after/plugin/completion.lua
+++ nvim/after/plugin/completion.lua
@@ -51,7 +51,23 @@ cmp.setup({
     { name = "nvim_lsp_signature_help" },
     { name = "luasnip" },
     { name = "yank_history", keyword_length = 3 },
-    { name = "buffer", keyword_length = 3 },
+    {
+      name = "buffer",
+      keyword_length = 3,
+      option = {
+        get_bufnrs = function()
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+            if byte_size <= 1024 * 1024 then -- Only index files smaller than 1MB
+              bufs[buf] = true
+            end
+          end
+          return vim.tbl_keys(bufs)
+        end,
+      },
+    },
     { name = "path" },
   }),
   formatting = {
