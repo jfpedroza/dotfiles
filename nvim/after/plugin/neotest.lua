b592a403ea94b8fc070a354c43cf8996fe81ec2e
Jhon Pedroza <jhon@pedroza.me>
2022-09-09 04:12:32 -0500

    [vim] Set Neotest run commands to read env vars from a global variable

--- nvim/after/plugin/neotest.lua
+++ nvim/after/plugin/neotest.lua
@@ -2,17 +2,40 @@ if not pcall(require, "neotest") then
   return
 end
 
-require("neotest").setup({
+local neotest = require("neotest")
+
+neotest.setup({
   adapters = {
     require("neotest-elixir"),
   },
 })
 
-vim.cmd([[
-command! NeotestSummary lua require("neotest").summary.toggle()
-command! NeotestFile lua require("neotest").run.run(vim.fn.expand("%"))
-command! Neotest lua require("neotest").run.run(vim.fn.getcwd())
-command! NeotestNearest lua require("neotest").run.run()
-command! NeotestAttach lua require("neotest").run.attach()
-command! NeotestOutput lua require("neotest").output.open()
-]])
+local function run(arg)
+  local default_env = vim.g.neotest_run_env or {}
+  local args
+
+  if type(arg) == "table" then
+    local env = arg.env or {}
+    arg.env = vim.tbl_extend("force", default_env, env)
+    args = arg
+  else
+    args = { arg, env = default_env }
+  end
+
+  neotest.run.run(args)
+end
+
+vim.api.nvim_create_user_command("NeotestFile", function()
+  run(vim.fn.expand("%"))
+end, {})
+
+vim.api.nvim_create_user_command("Neotest", function()
+  run(vim.fn.getcwd())
+end, {})
+
+vim.api.nvim_create_user_command("NeotestNearest", run, {})
+
+vim.api.nvim_create_user_command("NeotestLast", neotest.run.run_last, {})
+vim.api.nvim_create_user_command("NeotestAttach", neotest.run.attach, {})
+vim.api.nvim_create_user_command("NeotestSummary", neotest.summary.toggle, {})
+vim.api.nvim_create_user_command("NeotestOutput", neotest.output.open, {})
