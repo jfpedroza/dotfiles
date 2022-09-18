51cccd4ee4d047af66235a82619c9a24de166b7b
Jhon Pedroza <jhon@pedroza.me>
2022-09-18 01:37:45 -0500

    [vim] Change diagnostics signs

--- nvim/after/plugin/diagnostic.lua
+++ nvim/after/plugin/diagnostic.lua
@@ -21,5 +21,12 @@ vim.diagnostic.config({
   update_in_insert = false,
 })
 
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
