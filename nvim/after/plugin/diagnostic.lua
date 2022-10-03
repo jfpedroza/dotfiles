49aa4cd35a006b6be9a4b270da442a01135bb14a
Jhon Pedroza <jhon@pedroza.me>
2022-10-03 03:43:26 -0500

    [vim] Add mappings for going to the highest severity diagnostic

--- nvim/after/plugin/diagnostic.lua
+++ nvim/after/plugin/diagnostic.lua
@@ -28,5 +28,39 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
-vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
+-- Go to the next diagnostic, but prefer going to errors first
+local severity_levels = {
+  vim.diagnostic.severity.ERROR,
+  vim.diagnostic.severity.WARN,
+  vim.diagnostic.severity.INFO,
+  vim.diagnostic.severity.HINT,
+}
+
+local get_highest_error_severity = function()
+  for _, level in ipairs(severity_levels) do
+    local diags = vim.diagnostic.get(0, { severity = { min = level } })
+    if #diags > 0 then
+      return level, diags
+    end
+  end
+end
+
+vim.keymap.set("n", "[g", function()
+  vim.diagnostic.goto_prev({})
+end)
+
+vim.keymap.set("n", "[G", function()
+  vim.diagnostic.goto_prev({
+    severity = get_highest_error_severity(),
+  })
+end)
+
+vim.keymap.set("n", "]g", function()
+  vim.diagnostic.goto_next({})
+end)
+
+vim.keymap.set("n", "]G", function()
+  vim.diagnostic.goto_next({
+    severity = get_highest_error_severity(),
+  })
+end)
