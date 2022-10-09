58258773745c003512711f47cbef8b051cb6a406
Jhon Pedroza <jhon@pedroza.me>
2022-10-09 03:16:52 -0500

    [vim] Set find_file = true in NvimTree toggle

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -6,4 +6,6 @@ require("nvim-tree").setup({})
 
 local nt_api = require("nvim-tree.api")
 
-vim.keymap.set("n", "<F5>", nt_api.tree.toggle, { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<F5>", function()
+  nt_api.tree.toggle({ find_file = true })
+end, { desc = "Toggle file explorer" })
