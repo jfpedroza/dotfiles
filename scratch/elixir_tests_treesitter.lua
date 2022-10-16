e493a935b616cfdce1c5b33e0a2f47acdd531f21
Jhon Pedroza <jhon@pedroza.me>
2022-10-16 01:29:49 -0500

    [scratch] Update elixir_tests_treesitter.lua

--- scratch/elixir_tests_treesitter.lua
+++ scratch/elixir_tests_treesitter.lua
@@ -13,6 +13,11 @@ local query = [[
     (arguments ((string (quoted_content) @test.name)))
     (do_block)
   ) @test.definition
+
+  ;; Doctests
+  ;; The word doctest is included in the name to make it easier to notice
+  (call 
+    target: (identifier) @_target (#eq? @_target "doctest")) @test.name @test.definition
   ]]
 
 local tquery = vim.treesitter.parse_query("elixir", query)
@@ -22,17 +27,17 @@ local function get_tests(bufnr)
   local tree = parser:parse()[1]
 
   local results = {}
-  for id, node, metadata in tquery:iter_captures(tree:root(), bufnr, 0, -1) do
-    local text = vim.treesitter.get_node_text(node, bufnr)
-    local range = { node:range() }
-    local the_data = {
-      id = id,
-      text = text,
-      range = range,
-      metadata = metadata,
-    }
+  -- Pattern index, match table, metadata
+  for _, match, _ in tquery:iter_matches(tree:root(), bufnr, 0, -1) do
+    local texts = {}
 
-    table.insert(results, the_data)
+    for id, node in pairs(match) do
+      local capture_name = tquery.captures[id]
+      local text = vim.treesitter.get_node_text(node, bufnr)
+      texts[capture_name] = text
+    end
+
+    table.insert(results, texts)
   end
 
   return results
