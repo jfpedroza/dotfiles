fc4572809703a21eaeb512be894151e69402c229
Jhon Pedroza <jhon@pedroza.me>
2024-07-21 00:20:07 -0500

    [zsh] Remove Kerl config options, add more Elixir aliases

--- zsh/elixir.zsh
+++ zsh/elixir.zsh
@@ -1,10 +1,11 @@
 export KERL_BUILD_DOCS=yes
 export KERL_INSTALL_HTMLDOCS=no
 export KERL_INSTALL_MANPAGES=no
-export KERL_CONFIGURE_OPTIONS="--without-odbc"
 
 alias mix.dg='mix deps.get'
+alias mix.du='mix deps.update'
 alias mix.c='mix compile'
+alias mix.cf='mix compile --force'
 alias mix.t='mix test'
 alias mix.tf='mix test --failed'
 alias mix.tint='mix test.interactive'
